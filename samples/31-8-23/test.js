9b87a932b50149b5a568ab2a59712a768b547d88
Josh <joseph@vuedata.in>
2023-08-31 18:58:03 +0530

    inserted samples

---
+++ samples/31-8-23/test.js
@@ -0,0 +1,17 @@
+/*
+{ var x= "10";
+var y= "20";
+var z=x*=y;
+console.log("x:"+x);
+console.log("y:"+y);
+console.log("z:"+z);
+}
+*/
+ 
+
+    var n = prompt("Enter a number");
+for (var i=1;i<=n;i++){
+    console.log("Number is:" + i);
+}
+
+
