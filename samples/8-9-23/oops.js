21e32623c40861a406a6fae826adc4f36847c466
Josh <joseph@vuedata.in>
2023-09-10 18:35:25 +0530

    updated for friday

---
+++ samples/8-9-23/oops.js
@@ -0,0 +1,24 @@
+class AirPodsPro{}
+
+class case{
+    construcor (holder, cap, light){
+        this.holder=holder;
+        this.cap=cap;
+        this.light=light;
+    }
+}
+class pods{
+    constructor(mic, speaker,buds ){
+        this.mic=mic;
+        this.speaker=speaker;
+        this.buds=buds;
+    }
+    
+}
+class port{
+    constructor (externalsocket, Internalsocket){
+        this.externalsocket=externalsocket;
+        this.Internalsocket=Internalsocket;
+    }
+
+}
