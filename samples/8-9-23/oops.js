19b6eb115ed6be03cc56239cda5df296c1a032d5
Josh <joseph@vuedata.in>
2023-09-12 10:09:11 +0530

    monday

--- samples/8-9-23/oops.js
+++ samples/8-9-23/oops.js
@@ -1,7 +1,15 @@
-class AirPodsPro{}
+class AirPodsProGen1{
+    constructor (touchcontrol, Bandwidth){
+        this.touchcontrol=touchcontrol;
+        this.Bandwidth=Bandwidth;
+    }
+    listen(){
+        console.log("this is AirpodGen1 without ANC");
+    }
+}
 
 class case{
-    construcor (holder, cap, light){
+    constructor (holder, cap, light){
         this.holder=holder;
         this.cap=cap;
         this.light=light;
@@ -21,4 +29,28 @@ class port{
         this.Internalsocket=Internalsocket;
     }
 
+}
+
+// inheritance and polymorphism
+
+class AirPodsProGen2 extends AirPodsProGen1 {
+    constructor(ANC, touchcontrol, Bandwidth){
+        this.ANC=ANC;
+    super(touchcontrol, Bandwidth)
+    }
+listen(){
+    console.log("this is AirpodsGen2 with ANC" + this.ANC);
+}
+}
+
+// Abstraction
+
+class AirpodGen1 {
+    constructor (charge, pair)
+    charge(){
+        return "Now airpod is charging";
+    }
+    pair(){
+        return "Successfully connected with device";
+    }
 }
