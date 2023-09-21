94c23accadf6bf32075ddfb0ac6c7146cb37ddcd
Josh <joseph@vuedata.in>
2023-09-21 06:29:13 +0530

    updated

---
+++ samples/VS CODE/connecttomysql/connecttomysql/Program.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace connecttomysql
+
+{
+
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            string server = "localhost";
+            string database = "library";
+            string username = "root";
+            string password = "Vuedata@123";
+            string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "USERNAME=" + username + ";" + "PASSWORD="
++ password + ";";
+            MySqlConnection conn = new MySqlConnection (constring);
+            conn.Open ();
+            string query = "select * from author";
+            MySqlCommand cmd = new MySqlCommand (query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader ();  
+            while (reader.Read ())
+            {
+                Console.WriteLine(reader["Author_ID"]);
+                Console.WriteLine(reader["Author_Name"]);
+                Console.WriteLine(reader["Nationality"]); 
+            }
+
+            conn.Close ();
+        }
+    }
+}
